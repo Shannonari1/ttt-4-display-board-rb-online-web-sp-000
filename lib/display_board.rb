250e8c34628b4d9ddd7e7dfc4ee9355df0c8bf42
Shannon Young <shannon.ari1@gmail.com>
2020-09-07 18:35:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,10 +6,10 @@ def display_board(the_board)
 
   puts "   |   |   "
   puts "-----------"
-  puts "   |#{the_board[4]}|    "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
 
-  board = [ " "," "," "," ","X"," "," "," "," "]
+  board = [ " "," "," "," "," "," "," "," "," "]
   display_board(board)
